00ccbb8173be1794494b16fd1463120bfb907ef7
Vihar Makwana <makwanavihar22@gmail.com>
2026-07-24 21:06:00 +0530

    Modify Next.js config for export output and images
    Updated Next.js configuration to change output type and image optimization settings.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  typescript: {
-    ignoreBuildErrors: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  reactStrictMode: false,
 };
 
 export default nextConfig;
